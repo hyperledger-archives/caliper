fcc815055f80321a8acead35b617a5f666a99a5f
Haojun Zhou <zhouhaojun@huawei.com>
2017-08-17 15:24:23 +0800

    add drm test case

---
+++ benchmark/drm/query.js
@@ -0,0 +1,33 @@
+/**
+* Copyright 2017 HUAWEI. All Rights Reserved.
+*
+* SPDX-License-Identifier: Apache-2.0
+*
+*/
+
+
+'use strict'
+
+module.exports.info  = "querying digital items";
+
+var bc, contx;
+var itemIDs;
+module.exports.init = function(blockchain, context, args) {
+    if(!args.hasOwnProperty('itemIDs') || args['itemIDs'].length === 0) {
+        return Promise.reject(new Error("drm.query - 'itemIDs' is missed in the arguments"));
+    }
+
+    bc      = blockchain;
+    contx   = context;
+    itemIDs = args.itemIDs;
+    return Promise.resolve();
+}
+
+module.exports.run = function() {
+    var id  = itemIDs[Math.floor(Math.random()*(itemIDs.length))];
+    return bc.queryState(contx, 'drm', 'v0', id);
+}
+
+module.exports.end = function(results) {
+    return Promise.resolve();
+}
