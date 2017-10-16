34b2135dca917329bf00bc01cd280eda3512b77a
Haojun Zhou <zhouhaojun@huawei.com>
2017-10-16 18:27:36 +0800

    add sut select

--- src/gui/www/remotecontrol.php
+++ src/gui/www/remotecontrol.php
@@ -4,21 +4,15 @@
         $pwd  = "your password";
         $path = "your directory of caliper ";
         $host = "host's ip address"
-        $port = 22; // host's ssh port
+        $port = 22;
         set_time_limit( 0);
-        // clear data
-        $tmp = fopen("log.txt", "w");
-        fwrite($tmp, "");
-        fclose($tmp);
-        $tmp = fopen("demo.json", "w");
-        fwrite($tmp, "");
-        fclose($tmp);
 
         $connect = ssh2_connect($host, $port);
         $hasReport = false;
+
         if(ssh2_auth_password($connect, $user, $pwd)){
             // start the benchmark
-            $stream = ssh2_exec($connect, $path.'start.sh ' . $_GET['b']);
+            $stream = ssh2_exec($connect, $path.'start.sh ' . $_GET['b'] . $_GET['s']);
             stream_set_blocking($stream, true);
             //$stream_out = ssh2_fetch_stream($stream, SSH2_STREAM_STDIO);
             //$stream_err = ssh2_fetch_stream($stream, SSH2_STREAM_STDERR);
