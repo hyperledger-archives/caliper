9d32d0c0010830884d5c139beb7f9682e364d8ac
Haojun Zhou <zhouhaojun@huawei.com>
2017-10-16 18:28:08 +0800

    add sut select

--- src/gui/www/remotecontrol.php
+++ src/gui/www/remotecontrol.php
@@ -12,7 +12,7 @@
 
         if(ssh2_auth_password($connect, $user, $pwd)){
             // start the benchmark
-            $stream = ssh2_exec($connect, $path.'start.sh ' . $_GET['b'] . $_GET['s']);
+            $stream = ssh2_exec($connect, $path.'start.sh ' . $_GET['b'] . ' ' . $_GET['s']);
             stream_set_blocking($stream, true);
             //$stream_out = ssh2_fetch_stream($stream, SSH2_STREAM_STDIO);
             //$stream_err = ssh2_fetch_stream($stream, SSH2_STREAM_STDERR);
