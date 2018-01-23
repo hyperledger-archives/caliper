aff05d45ae891c9bb5a25b98189675a33cc51239
Haojun Zhou <zhouhaojun@huawei.com>
2018-01-23 11:21:00 +0800

    1. update configuration file 2. add support for simulating contract

---
+++ src/contract/iroha/simple/factory.js
@@ -0,0 +1,19 @@
+/**
+* Copyright 2017 HUAWEI. All Rights Reserved.
+*
+* SPDX-License-Identifier: Apache-2.0
+*
+* @file, definition of the Fabric class, which implements the caliper's NBI for hyperledger fabric
+*/
+
+
+'use strict'
+
+module.exports.factory = {
+    simple : simple
+};
+
+var simple = function(version, args) {
+
+}
+
---
+++ src/iroha/type.js
@@ -0,0 +1,30 @@
+/**
+* Copyright 2017 HUAWEI. All Rights Reserved.
+*
+* SPDX-License-Identifier: Apache-2.0
+*
+* @file, definition of the Fabric class, which implements the caliper's NBI for hyperledger fabric
+*/
+
+
+'use strict'
+
+module.exports.Transactions = {
+    ADD_ASSET_QUANTITY : 1,
+    ADD_PEER : 2,
+    ADD_SIGATORY : 3,
+    APPEND_ROLE : 4,
+    CREATE_ACCOUNT : 5,
+    CREATE_ASSET : 6,
+    CREATE_DOMAIN : 7,
+    CREATE_ROLE : 8,
+    DETACH_ROLE : 9,
+    GRANT_PERMISSION : 10,
+    REMOVE_SIGNATORY : 11,
+    REVOKE_PERMISSION : 12,
+    SET_ACCOUNT_DETAIL : 13,
+    SET_ACCOUNT_QUORUM : 14,
+    SUBTRACT_ASSET_QUANTITY : 15,
+    TRANSFER_ASSET : 16
+};
+
